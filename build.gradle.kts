b72549ff2754c74c6291e06a7cc93f9568bb3f5e
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-04-12 10:15:20 +0200

    uprade deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 
 val jvmTarget = "17"
-val micronautVersion="3.8.6"
+val micronautVersion="3.8.7"
 val micrometerRegistryPrometheusVersion = "1.9.1"
 val junitJupiterVersion = "5.9.0"
 val jacksonVersion = "2.13.4"
-val logbackClassicVersion = "1.2.11"
-val logbackEncoderVersion = "7.2"
-val mockkVersion = "1.13.2"
-val kotestVersion = "5.5.0"
+val logbackClassicVersion = "1.4.6"
+val logbackEncoderVersion = "7.3"
+val mockkVersion = "1.13.4"
+val kotestVersion = "5.5.5"
 val openSearchRestClientVersion = "2.4.1"
 
 group = "no.nav.hm"
@@ -19,10 +19,9 @@ version = properties["version"] ?: "local-build"
 plugins {
     kotlin("jvm") version "1.7.0"
     kotlin("kapt") version "1.7.0"
-    kotlin("plugin.allopen") version "1.7.0"
     id("java")
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("io.micronaut.application") version "3.7.2"
+    id("io.micronaut.application") version "3.7.4"
 }
 
 configurations.all {
@@ -49,7 +48,7 @@ dependencies {
     testImplementation("io.mockk:mockk:$mockkVersion")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.2")
 }
 
 micronaut {
