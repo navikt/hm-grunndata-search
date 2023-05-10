0e5ee2e74e6d634ab5b6e0bf3085ea84e59bd648
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-05-10 12:41:26 +0200

    more logs

--- src/main/kotlin/no/nav/hm/grunndata/search/DocApi.kt
+++ src/main/kotlin/no/nav/hm/grunndata/search/DocApi.kt
@@ -13,24 +13,23 @@ class DocApi(private val searchService: SearchService) {
         private val LOG = LoggerFactory.getLogger(DocApi::class.java)
     }
 
-
     @Get(uris=["/products/_doc/{id}{?params*}"])
     fun getProductById(params: Map<String, String>?, id: String): HttpResponse<String> {
-        LOG.info("Got get lookup request for product $params $id")
+        LOG.info("Got lookup request for product $id")
         return HttpResponse.ok(searchService.lookupWithQuery(SearchService.PRODUCTS, params, id))
             .header(CACHE_CONTROL, "public, immutable, max-age=3600")
     }
 
     @Get(uris=["/agreements/_doc/{id}{?params*}"])
     fun searchAgreementWithQuery(params: Map<String, String>?, id: String): HttpResponse<String> {
-        LOG.info("Got get lookup request for agreement $params $id ")
+        LOG.info("Got lookup request for agreement $id ")
         return HttpResponse.ok(searchService.lookupWithQuery(SearchService.AGREEMENTS, params, id))
             .header(CACHE_CONTROL, "public, immutable, max-age=3600")
     }
 
     @Get(uris=["/suppliers/_doc/{id}{?params*}"])
     fun searchSupplierWithQuery(params: Map<String, String>?, id:String): HttpResponse<String> {
-        LOG.info("Got get lookup request for supplier $params $id")
+        LOG.info("Got lookup request for supplier $id")
         return HttpResponse.ok(searchService.lookupWithQuery(SearchService.SUPPLIERS, params,id))
             .header(CACHE_CONTROL, "public, immutable, max-age=3600")
     }
--- src/main/kotlin/no/nav/hm/grunndata/search/SearchApi.kt
+++ src/main/kotlin/no/nav/hm/grunndata/search/SearchApi.kt
@@ -15,12 +15,13 @@ class SearchApi(private val searchService: SearchService) {
 
     @Post(uris=["/products/_search{?params*}"])
     fun searchProductWithBody(@QueryValue params: Map<String, String>?, @Body body: String): HttpResponse<String> {
+        LOG.info("Got body request for product")
         return HttpResponse.ok(searchService.searchWithBody(SearchService.PRODUCTS, params, body))
     }
 
     @Get(uris=["/products/_search{?params*}"])
     fun searchProductWithQuery(params: Map<String, String>?): HttpResponse<String> {
-        LOG.info("Got get request for product $params")
+        LOG.info("Got query request for product")
         return HttpResponse.ok(searchService.searchWithQuery(SearchService.PRODUCTS, params))
             .header(CACHE_CONTROL, "public, immutable, max-age=3600")
     }
@@ -28,24 +29,26 @@ class SearchApi(private val searchService: SearchService) {
 
     @Post(uris=["/agreements/_search{?params*}"])
     fun searchAgreementWithBody(@QueryValue params: Map<String, String>?, @Body body: String): HttpResponse<String> {
+        LOG.info("Got body request for agreement")
         return HttpResponse.ok(searchService.searchWithBody(SearchService.AGREEMENTS, params, body))
     }
 
     @Get(uris=["/agreements/_search{?params*}"])
     fun searchAgreementWithQuery(params: Map<String, String>?): HttpResponse<String> {
-        LOG.info("Got get request for product $params")
+        LOG.info("Got query request for agreement")
         return HttpResponse.ok(searchService.searchWithQuery(SearchService.AGREEMENTS, params))
             .header(CACHE_CONTROL, "public, immutable, max-age=3600")
     }
 
     @Post(uris=["/suppliers/_search{?params*}"])
     fun searchSupplierWithBody(@QueryValue params: Map<String, String>?, @Body body: String): HttpResponse<String> {
+        LOG.info("Got body request for supplier")
         return HttpResponse.ok(searchService.searchWithBody(SearchService.SUPPLIERS, params, body))
     }
 
     @Get(uris=["/suppliers/_search{?params*}"])
     fun searchSupplierWithQuery(params: Map<String, String>?): HttpResponse<String> {
-        LOG.info("Got get request for product $params")
+        LOG.info("Got query request for supplier")
         return HttpResponse.ok(searchService.searchWithQuery(SearchService.SUPPLIERS, params))
             .header(CACHE_CONTROL, "public, immutable, max-age=3600")
     }
