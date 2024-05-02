ad6d23845f46f71c31c115c2035af244cc269554
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2024-05-02 13:30:21 +0200

    Api endpoint for fetching updates since a certain date from the external_products index

--- src/main/kotlin/no/nav/hm/grunndata/search/SearchApi.kt
+++ src/main/kotlin/no/nav/hm/grunndata/search/SearchApi.kt
@@ -1,12 +1,20 @@
 package no.nav.hm.grunndata.search
 
+import com.fasterxml.jackson.annotation.JsonAlias
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.micronaut.http.HttpHeaders.CACHE_CONTROL
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.*
 import org.slf4j.LoggerFactory
+import java.time.LocalDate
 
 @Controller
-class SearchApi(private val searchService: SearchService) {
+class SearchApi(
+    private val searchService: SearchService,
+    private val objectMapper: ObjectMapper,
+) {
     companion object {
         private val LOG = LoggerFactory.getLogger(SearchApi::class.java)
     }
@@ -61,5 +69,61 @@ class SearchApi(private val searchService: SearchService) {
         LOG.info("Got query request for news")
         return HttpResponse.ok(searchService.searchWithQuery(SearchService.NEWS, params))
             .header(CACHE_CONTROL, "public, immutable, max-age=300")
+    }
+
+    @Post(uris=["/external_products{?params*}"])
+    fun getExternalProducts(@QueryValue params: Map<String, String>): HttpResponse<String> {
+        data class OSResponseHit(
+            @JsonAlias("_source")
+            val externalProduct: JsonNode,
+        )
+        data class OSResponseTotal(
+            val value: Int,
+        )
+        data class OSResponseHits(
+            val hits: List<OSResponseHit>?,
+            val total: OSResponseTotal,
+        )
+        data class OSResponse(
+            val hits: OSResponseHits?,
+        )
+        data class Response(
+            val total: Int,
+            val results: List<JsonNode>,
+        )
+
+        val since = params["since"]?.let { LocalDate.parse(it) } ?: LocalDate.now().minusDays(1)
+        val from = params["from"]?.toIntOrNull() ?: 0
+        val size = params["size"]?.toIntOrNull() ?: 1000
+
+        LOG.info("Got request for external_products (start=$since, from=$from, size=$size)")
+
+        val query = """
+            {
+                "query": {
+                    "range": {
+                        "updated": {
+                            "gte": "$since",
+                            "format": "yyyy-MM-dd"
+                        }
+                    }
+                },
+                "sort": [
+                    {
+                        "updated": {
+                            "order": "asc"
+                        }
+                    }
+                ],
+                "from": $from,
+                "size": $size
+            }
+        """.trimIndent()
+
+        val results: OSResponse = objectMapper.readValue(searchService.searchWithBody(SearchService.EXTERNAL_PRODUCTS, mapOf(), query))
+        return HttpResponse.ok(objectMapper.writeValueAsString(Response(
+            total = results.hits?.total?.value ?: 0,
+            results = results.hits?.hits?.map { it.externalProduct } ?: listOf()
+        )))
     }
 }
