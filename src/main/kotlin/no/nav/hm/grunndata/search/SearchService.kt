0821a702c5ebd7e4d8821eae531ebe2b02851ce2
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-05-08 09:03:20 +0200

    refactor

--- src/main/kotlin/no/nav/hm/grunndata/search/SearchService.kt
+++ src/main/kotlin/no/nav/hm/grunndata/search/SearchService.kt
@@ -7,62 +7,51 @@ import org.apache.http.entity.StringEntity
 import org.apache.http.util.EntityUtils
 import org.opensearch.client.Request
 import org.slf4j.LoggerFactory
-import java.net.ConnectException
 import org.opensearch.client.RestClient
 
 @Singleton
-class SearchService (private val osclient: RestClient) {
+class SearchService(private val osclient: RestClient) {
 
     fun searchWithBody(index: String, params: Map<String, String>?, body: String): String {
-        if (params!=null) require(onlyAllowedParams(params)) { "Disallowed request params present in " + params.keys }
-        return try {
-            val entity = StringEntity(body, ContentType.APPLICATION_JSON)
-            val request: Request = newRequest("POST", "/$index/_search", params, entity)
-            val httpEntity: HttpEntity = osclient.performRequest(request).entity
-            EntityUtils.toString(httpEntity)
-        } catch (e: ConnectException) {
-            LOG.error("No connection to Opensearch", e)
-            throw e
-        }
+        val entity = StringEntity(body, ContentType.APPLICATION_JSON)
+        val request: Request = newRequest("POST", "/$index/_search", params, entity)
+        return performRequest(request, params)
     }
+
 
     fun searchWithQuery(index: String, params: Map<String, String>?): String {
-        if (params!=null) require(onlyAllowedParams(params)) { "Disallowed request params present in " + params.keys }
-        return try {
-            val request: Request = newRequest("GET", "/$index/_search", params, null)
-            val responseEntity: HttpEntity = osclient.performRequest(request).entity
-            EntityUtils.toString(responseEntity)
-        } catch (e: ConnectException) {
-           LOG.error("No connection to Opensearch", e)
-            throw e
-        }
+        val request: Request = newRequest("GET", "/$index/_search", params, null)
+        return performRequest(request, params)
     }
 
-    fun lookupWithQuery(index:String, params: Map<String, String>?, id: String): String {
-        if (params!=null) require(onlyAllowedParams(params)) { "Disallowed request params present in " + params.keys }
+    fun lookupWithQuery(index: String, params: Map<String, String>?, id: String): String {
+        val request: Request = newRequest("GET", "/$index/_doc/$id", params, null)
+        return performRequest(request, params)
+    }
+
+    private fun performRequest(request: Request, params: Map<String, String>?): String {
+        if (params != null) require(onlyAllowedParams(params)) { "Disallowed request params present in " + params.keys }
         return try {
-            val request: Request = newRequest("GET", "/$index/_doc/$id", params, null)
             val responseEntity: HttpEntity = osclient.performRequest(request).entity
             EntityUtils.toString(responseEntity)
-        } catch (e: ConnectException) {
-            LOG.error("No connection to Opensearch", e)
+        } catch (e: Exception) {
+            LOG.error("Connection error to Opensearch", e)
             throw e
         }
     }
 
     fun sqlQuery(params: Map<String, String>?, query: String): String {
-        return try {
-            val entity = StringEntity(query, ContentType.APPLICATION_JSON)
-            val request: Request = newRequest("POST", "/_plugins/_sql", params, entity)
-            val responseEntity: HttpEntity = osclient.performRequest(request).entity
-            EntityUtils.toString(responseEntity)
-        } catch (e: ConnectException) {
-            LOG.error("No connection to Opensearch", e)
-            throw e
-        }
+        val entity = StringEntity(query, ContentType.APPLICATION_JSON)
+        val request: Request = newRequest("POST", "/_plugins/_sql", params, entity)
+        return performRequest(request, params)
     }
 
-    private fun newRequest(method: String, endpoint: String, params: Map<String, String>?, entity: StringEntity? ): Request {
+    private fun newRequest(
+        method: String,
+        endpoint: String,
+        params: Map<String, String>?,
+        entity: StringEntity?
+    ): Request {
         val request = Request(method, endpoint)
         params?.forEach(request::addParameter)
         request.entity = entity
@@ -73,6 +62,7 @@ class SearchService (private val osclient: RestClient) {
     private fun onlyAllowedParams(params: Map<String, String>): Boolean {
         return ALLOWED_REQUEST_PARAMS.containsAll(params.keys)
     }
+
     companion object {
         const val PRODUCTS = "products"
         const val EXTERNAL_PRODUCTS = "external_products"
@@ -83,6 +73,5 @@ class SearchService (private val osclient: RestClient) {
         private val LOG = LoggerFactory.getLogger(SearchService::class.java)
         val ALLOWED_REQUEST_PARAMS = setOf("q", "filter_path", "pretty")
     }
-
 
 }
